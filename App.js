7cad32375999bf5b31e9ab167f812c00fc3db327
xurros <36108362+xurros@users.noreply.github.com>
2022-04-16 16:45:22 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from "react-native";
+// import { StyleSheet } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
@@ -9,47 +9,35 @@ import "react-native-gesture-handler";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import "@react-navigation/native-stack";
 
 // This is to eliminate the yellow warning from react-native
 import { LogBox } from "react-native";
+
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+  "AsyncStorage",
+  "Animated",
 ]);
 
+export default function App() {
+  const Stack = createStackNavigator();
 
-// create navigator
-const Stack = createStackNavigator();
+  return (
 
+    < NavigationContainer >
+      <Stack.Navigator
+        initialRouteName="Start">
+        <Stack.Screen
+          name="Start"
+          component={Start} />
 
-export default class App extends React.Component {
-
-  render() {
-    return (
-      <NavigationContainer>
-
-        <Stack.Navigator
-          initialRouteName="Start">
-          <Stack.Screen
-            name="Start"
-            component={Start} />
-
-          <Stack.Screen
-            name="Chat"
-            component={Chat} />
-        </Stack.Navigator>
-
-      </NavigationContainer>
-    );
-  }
+        <Stack.Screen
+          name="Chat"
+          component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer >
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
 
